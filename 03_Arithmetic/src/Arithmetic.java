1cb02c8cf576f5625c16a120fd3b07cf4d5704da
aleksandr_parshakov <sash29@yandex.ru>
2021-10-11 21:02:58 +0500

    add constructor and method's

--- 03_Arithmetic/src/Arithmetic.java
+++ 03_Arithmetic/src/Arithmetic.java
@@ -1,5 +1,17 @@
 public class Arithmetic {
 
+    public static void main(String[] args) {
+        Arithmetic summa = new Arithmetic();
+        int summaResult = summa.amountNum(5, 2);
+        System.out.println("Сумма " + summaResult);
+        Arithmetic product = new Arithmetic();
+        int productNumber = product.productNum(5, 2);
+        System.out.println("Произведение " + productNumber);
+        Arithmetic bigger = new Arithmetic();
+        int biggerResult = bigger.biggerNum(5, 2);
+        System.out.println("Большее значение " + biggerResult);
+    }
+
     public int amountNum(int first, int second) {
         int result = first + second;
         return result;
@@ -10,14 +22,14 @@ public class Arithmetic {
         return result;
     }
 
-    public static void main(String[] args) {
-        Arithmetic summa = new Arithmetic();
-        int summaResult = summa.amountNum(5, 2);
-        System.out.println("Сумма " + summaResult);
-        Arithmetic product = new Arithmetic();
-        int productNumber = product.productNum(5, 2);
-        System.out.println("Произведение " + productNumber);
+    public int biggerNum(int a, int b) {
+        if (a > b) {
 
+        } else {
+            System.out.println("Меньшее значение " + b);
+        }
+
+        return a;
     }
-
 }
+
