aa1c93291037cd01e57dc80155e2f790c3fd59eb
aleksandr_parshakov <sash29@yandex.ru>
2021-10-26 23:21:22 +0500

    add class and method's

---
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -0,0 +1,44 @@
+package ru.skillbox;
+
+public class Elevator {
+
+    private int minFloor;
+
+    private int maxFloor;
+
+    public int currentFloor = 1;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    public void moveUp() {
+        currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor;
+    }
+
+    public void moveDown() {
+        currentFloor = (currentFloor > 1) ? currentFloor: - 1;
+    }
+
+    public void move(int floor) {
+        if (floor < minFloor || floor > maxFloor || floor == 0) {
+            System.out.print("Ошибка: Этаж введен не верно! ");
+            return;
+        }
+        if (currentFloor < floor)
+            while (currentFloor != floor) {
+                moveUp();
+                System.out.println("Текущий этаж: " + currentFloor);
+            }
+        if (currentFloor > floor)
+            while (currentFloor != floor) {
+                moveDown();
+                System.out.println("Текущий этаж: " + currentFloor);
+            }
+    }
+
+}
