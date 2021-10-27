d896cad71a1c7892f9e4a01ce83b8dafe60d99fb
aleksandr_parshakov <sash29@yandex.ru>
2021-10-27 19:11:44 +0500

    add class, add get, add set, add constructor

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Country.java
@@ -0,0 +1,66 @@
+package ru.skillbox;
+
+public class Country {
+        private String name;
+        private int population;
+        private double square;
+        private String capitalName;
+        private String seaAccess;
+        private String title;
+
+    public Country(String name, int population, double square, String capitalName, String seaAccess) {
+        this.name = name;
+        this.population = population;
+        this.square = square;
+        this.capitalName = capitalName;
+        this.seaAccess = seaAccess;
+    }
+
+    public void add(String name, int population, double square, String capitalName, String seaAccess) {
+        title = title + "\n" + name + population + "тыс. чел." + square + "км/2" + capitalName + seaAccess;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+
+    public String getSeaAccess() {
+        return seaAccess;
+    }
+
+    public void setSeaAccess(String seaAccess) {
+        this.seaAccess = seaAccess;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
@@ -3,5 +3,8 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Country country = new Country("Губаха", 18, 58, "Пермь", "Нет");
+        country.add("Губаха", 18, 58, "Пермь", "Нет");
+
     }
 }
