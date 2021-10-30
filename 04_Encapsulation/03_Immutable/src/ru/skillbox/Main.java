7aea4cc19d15aaad0a2ddab82efe4471c993a066
aleksandr_parshakov <sash29@yandex.ru>
2021-10-30 14:53:29 +0500

    changes country add Book, Product

--- 04_Encapsulation/03_Immutable/src/ru/skillbox/Main.java
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Main.java
@@ -3,7 +3,7 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
-        Book book = new Book("Sana", "Porsche", 255, 777777);
+        Book book = new Book("", "", 255, 777777);
         System.out.println(book);
         Product product = new Product("Name");
         System.out.println(product);
