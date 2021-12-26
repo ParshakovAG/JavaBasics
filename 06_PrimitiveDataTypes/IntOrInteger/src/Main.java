6e4eb8c7f46565a2762364bca3dea7e6a5482dec
aleksandr_parshakov <sash29@yandex.ru>
2021-12-26 20:27:30 +0500

    add convert to char

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -4,9 +4,17 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        System.out.println();
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        for (int i = 1040; i < 1104; i++) {
+            char c = (char) i;
+
+            System.out.println(i + " - " + c);
+        }
     }
+
 }
