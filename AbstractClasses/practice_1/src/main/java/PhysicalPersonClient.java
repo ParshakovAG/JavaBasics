a54df8daf8b1033edb7140a85cc73ff3aa2603c4
Александр Паршаков <sash29@yandex.ru>
2022-08-18 17:21:35 +0000

    Update PhysicalPersonClient.java

--- AbstractClasses/practice_1/src/main/java/PhysicalPersonClient.java
+++ AbstractClasses/practice_1/src/main/java/PhysicalPersonClient.java
@@ -1,2 +1,13 @@
 public class PhysicalPersonClient extends Client {
+
+    protected PhysicalPersonClient() {
+    }
+
+    protected PhysicalPersonClient(double amount) {
+        super(amount);
+    }
+    @Override
+    protected void info() {
+        System.out.println("Пополнение и снятие без комиссии");
+    }
 }
