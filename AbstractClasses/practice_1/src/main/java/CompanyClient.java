0e7de43b537e7e6702b976d68d293be79bc4deaa
Александр Паршаков <sash29@yandex.ru>
2022-08-18 17:20:46 +0000

    Update CompanyClient.java

--- AbstractClasses/practice_1/src/main/java/CompanyClient.java
+++ AbstractClasses/practice_1/src/main/java/CompanyClient.java
@@ -1,2 +1,19 @@
-public class CompanyClient extends Client {
+public class CompanyClient extends PhysicalPersonClient {
+
+    private final static double FEE = 0.01;
+
+    @Override
+    public void take(double amountTake) {
+        super.take(amountTake + getAmountWithCommission(amountTake));
+    }
+
+    protected double getAmountWithCommission(double amount) {
+        return amount * FEE;
+    }
+    @Override
+    protected void info() {
+
+        System.out.println("Пополнение без комиссии");
+        System.out.println("Снятие - комиссия 1%");
+    }
 }
